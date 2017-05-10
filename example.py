6a5f11c06a12d9600b9aca8473d9c6e5b1be6661
Stephan P. A. Sauer <spasauer@users.noreply.github.com>
2017-05-10 15:00:22 +0200

    change dog to cat in example.py

--- example.py
+++ example.py
@@ -9,7 +9,7 @@ def reverse_words(s):
 
 def test_reverse_words():
     assert reverse_words('dogs hate cats') == 'cats hate dogs'
-    assert reverse_words('dog eat dog') == 'dog eat dog'
+    assert reverse_words('cat eat cat') == 'cat eat cat'
     assert reverse_words('one two three four') == 'four three two one'
 
 
